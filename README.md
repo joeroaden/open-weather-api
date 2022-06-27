a0fd67c74560cb54a97c54682a99db2475ba4d42
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-27 14:15:02 -0700

    add zip code search functionality, add jquery options
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Setup/Installation Requirements
 * Clone this repository to your desktop. 
 * Create your own local .env file. 
-* Add youe .env file to your .gitignore file.
+* Add your .env file to your .gitignore file.
 * Navigate to ??? and create your own API.
 * Add your API key variable to your .env file.
 * Run $ npm install.
--- src/index.js
+++ src/index.js
@@ -8,14 +8,16 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
+    $('#zip').val("");
+
 
     let request = new XMLHttpRequest();
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+        const response1 = JSON.parse(this.responseText);
+        getElements(response1);
       }
     };
 
@@ -23,9 +25,33 @@ $(document).ready(function() {
     request.send();
 
     function getElements(response) {
+      $('.showName').text(`City: ${response.name}`);
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Fahrenheit is ${converter(response.main.temp)} degrees.`);
       $('.showWind').text(`The wind speed is ${response.wind.speed} m/s.`);
+    }
+  });
+  $('#weatherLocationZip').click(function(){
+    const zip = $('#zip').val();
+    $('#zip').val("");
+    let request1 = new XMLHttpRequest();
+    const url2 = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${process.env.API_KEY}`;
+
+    request1.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response1 = JSON.parse(this.responseText);
+        getElements(response1);
+      }
+    };
+
+    request1.open("GET", url2, true);
+    request1.send();
+
+    function getElements(response1) {
+      $('.showName').text(`City: ${response1.name}`);
+      $('.showHumidity').text(`The humidity in ${zip} is ${response1.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${converter(response1.main.temp)} degrees.`);
+      $('.showWind').text(`The wind speed is ${response1.wind.speed} m/s.`);
     }
   });
 });
